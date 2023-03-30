ee73b09442c9269756cbe1bf4093bfbeb1ec1409
Everson Araujo <araujoeverson@icloud.com>
2023-03-29 23:57:45 -0300

    video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -6,7 +6,8 @@ function Video() {
             <video 
             className='video__player'
             controls
-            src='https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2'
+            loop
+            src='https://v16-webapp-prime.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c004/o8CeAcmedeTQlI7gQGDnbvb4ozZ3gl1ZCS3nqj/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=6060&bt=3030&cs=0&ds=3&ft=_RwJrBiTq8ZmoLS-Cc_vjt-I8AhLrus&mime_type=video_mp4&qs=0&rc=ZGQ1Z2hnNDU3NzwzM2U5OUBpM21lZWc6ZnN1ZzMzNzczM0A2X18wLjA0NmIxXjItLl9hYSNiXjRtcjRvZnFgLS1kMTZzcw%3D%3D&btag=80000&expire=1680166269&l=20230330025046E67D2686F8C7178E903E&ply_type=2&policy=2&signature=a7b1b1119b6ec9816b7a5b8783397a57&tk=tt_chain_token'
         >
 
         </video>
@@ -15,3 +16,4 @@ function Video() {
 }
 
 export default Video
+
