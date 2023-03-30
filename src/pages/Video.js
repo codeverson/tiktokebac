a6a45be92737a583df07b2abc3792c4c290274be
Everson Araujo <araujoeverson@icloud.com>
2023-03-30 00:49:12 -0300

    50:13

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./video.css";
-import mp4 from "./mp4/Cremosinho.mp4";
-
+import mp4 from "../mp4/Cremosinho.mp4";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handleStart() {
+    if (play == false) {
+      videoRef.current.play();
+      setPlay(true);
+    }
+
+    if (play == true) {
+      videoRef.current.pause();
+      setPlay(false);
+    }
+  }
+
   return (
     <div className="video">
       <video
         className="video__player"
-        controls
+        ref={videoRef}
+        onClick={handleStart}
         loop
         src={mp4}
       ></video>
     </div>
   );
-}
+} 
 
 export default Video;
