1a2ac3f16d1e7208a8630ddfaa2b1e77ff29ae95
Everson Araujo <araujoeverson@icloud.com>
2023-04-02 17:28:53 -0300

    final aula 5

--- src/App.js
+++ src/App.js
@@ -33,16 +33,6 @@ function App() {
             />
           );
         })}
-
-        {/* <Video 
-        likes={444}
-        messages={555} 
-        shares={666}
-        name="otiehit" 
-        description="Saiu o hit do verão!! Assista agora no Youtube! #tierry #manoelgomes" 
-        music="Lá Ele - Tierry & Manoel Gomes"
-        url="https://firebasestorage.googleapis.com/v0/b/tiktok-clone-7601f.appspot.com/o/laele.mp4?alt=media&token=a579c7a7-ed31-4645-a1c0-6945fa1ba855"
-        /> */}
       </div>
     </div>
   );
